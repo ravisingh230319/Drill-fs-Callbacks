350b62a22318163c03e7ec360b0765d910537e3f
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-18 10:28:41 +0530

    Formatting corrected with solutions

--- problem1.js
+++ problem1.js
@@ -1,7 +1,29 @@
 const fs = require("fs");
 
-const problem1 = (cb) => {
-    fs.mkdir("../JSON", cb);
+const problem1 = () => {
+    fs.mkdir("../JSON", () => {
+        for (let i = 0; i < 8; i++) {
+            const path ="../JSON/" + Math.random().toString(36).substr(2, 5) + ".json";
+    
+            //Creating random files
+            fs.writeFile(path, "Content", (error) => {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log("File created  " + path);
+                }
+    
+                //Deleting files inside the callback function of writeFile
+                fs.unlink(path, (error) => {
+                    if (error) {
+                        throw error;
+                    } else {
+                        console.log("File deleted  " + path);
+                    }
+                });
+            });
+        }
+    });
 };
 
 module.exports = problem1;
--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,11 +1,4 @@
 const fs = require("fs");
 const problem2 = require("../problem2.js");
 
-
-
-const deleteFiles = (filePath) => {
-    fs.unlink(filePath, (error) => {
-        if (error) throw error;
-    });
-};
 problem2();
